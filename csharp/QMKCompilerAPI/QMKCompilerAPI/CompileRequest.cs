82e46e768a0b3f5909decac95a3460551ca11b88
Mitchell van Manen <ma.van.manen@live.nl>
2018-02-07 20:42:07 +0100

    change layers to a twodimensional array

--- csharp/QMKCompilerAPI/QMKCompilerAPI/CompileRequest.cs
+++ csharp/QMKCompilerAPI/QMKCompilerAPI/CompileRequest.cs
@@ -1,17 +1,16 @@
 ﻿using System.Collections.Generic;
+using System.Collections.Specialized;
 
 namespace QMKCompilerAPI
 {
-    public class Layers : List<List<string>> { }
-
     public class CompileRequest
     {
         public string Keyboard { get; set; }
         public string Keymap { get; set; }
         public string Layout { get; set; }
-        public Layers Layers { get; set; }
+        public string[][] Layers { get; set; }
 
-        public CompileRequest(Keyboard keyboard, string keymap, string layout, Layers layers)
+        public CompileRequest(Keyboard keyboard, string keymap, string layout, string[][] layers)
         {
             Keyboard = keyboard.KeyboardName;
             Keymap = keymap;
