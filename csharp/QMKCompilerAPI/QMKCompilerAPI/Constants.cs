908422de1495e863a7a4f22207ecd0859f85aa26
Mitchell van Manen <ma.van.manen@live.nl>
2018-02-07 20:16:04 +0100

    Add constants for the compile endpoints

--- csharp/QMKCompilerAPI/QMKCompilerAPI/Constants.cs
+++ csharp/QMKCompilerAPI/QMKCompilerAPI/Constants.cs
@@ -4,9 +4,12 @@
     public static class Constants
     {
         public const string KEYBOARD_PARAM = "keyboard";
+        public const string COMPILE_CHECK_PARAM = "jobid";
 
         public const string BASE = "http://compile.qmk.fm/v1";
         public const string KEYBOARDS = "/keyboards";
         public const string KEYBOARD = KEYBOARDS + "/{" + KEYBOARD_PARAM + "}";
+        public const string COMPILE = "/compile";
+        public const string COMPILE_CHECK = "/compile/{" + COMPILE_CHECK_PARAM + "}";
     }
 }
